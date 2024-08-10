a1ec59863e664cd08b30b2f3173eaf7b2abd9870
fauzan.firzandy <fauzan.firzandy@ui.ac.id>
2024-08-10 15:48:15 +0700

    Initial Commit

---
+++ TestUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestUIKit
+//
+//  Created by Fauzan Firzandy on 10/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
